97d8f5941dc8f1aee4beb0804b731505728d28cb
Nick Ohman <nickohman@Nicks-MacBook-Pro.local>
2020-06-11 21:14:39 -0500

    MVP

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const helmet = require('helmet');
 
+const RecipesRouter = require('./recipes/recipes-router.js');
 const server = express();
 
 
 server.use(helmet());
 server.use(express.json());
+
+server.use('/api', RecipesRouter)
 
 
 
